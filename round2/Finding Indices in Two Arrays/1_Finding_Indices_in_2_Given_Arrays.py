61d214ba42c8c1535917fd233e18607805758162
Shailesh Arya <aryashailesh12@gmail.com>
2023-09-24 15:03:14 +0530

    updated code

--- round2/Finding Indices in Two Arrays/1_Finding_Indices_in_2_Given_Arrays.py
+++ round2/Finding Indices in Two Arrays/1_Finding_Indices_in_2_Given_Arrays.py
@@ -1,5 +1,12 @@
 #Finding indices of elements from given array1 within array2
 
+# input_str1 = input("Enter elements for the first array separated by spaces: ")
+# input_str2 = input("Enter elements for the second array separated by spaces: ")
+
+# # split seperated by spaces
+# arr1 = list(map(int, input_str1.split()))
+# arr2 = list(map(int, input_str2.split()))
+
 arr1 = [1,2,3]
 arr2 = [1,6,2,5,3,1,4]
 
@@ -15,7 +22,6 @@ for num in arr1:
     if num in indices_dict:
         print(f"Element {num} from array1 is found at indices {indices_dict[num]} in array2.")
 
-# print(indices_dict)
 
 # for i, num1 in enumerate(arr1):
 #     for j, num2 in enumerate(arr2):
